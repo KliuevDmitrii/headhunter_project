e9d08a1d240f27807915729c532d77c6c1780660
KliuevDmitrii <dimik1986@gmail.com>
2025-12-16 00:06:15 +0400

    add new notifier

--- hh_bump/notifier.py
+++ hh_bump/notifier.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from pathlib import Path
 
 
 class TelegramNotifier:
@@ -12,27 +13,34 @@ class TelegramNotifier:
 
         self.base_url = f"https://api.telegram.org/bot{self.token}"
 
-    def send(self, text: str):
+    def send(self, text: str, file_path: Path | None = None):
         """
-        Отправка текстового сообщения
+        Отправляет сообщение в Telegram.
+        Если передан file_path — отправляет файл вместе с сообщением.
         """
+
+        if file_path:
+            self._send_document(text, file_path)
+        else:
+            self._send_message(text)
+
+    def _send_message(self, text: str):
         url = f"{self.base_url}/sendMessage"
         payload = {
             "chat_id": self.chat_id,
             "text": text,
+            "parse_mode": "HTML",
         }
-
         r = requests.post(url, json=payload, timeout=20)
         r.raise_for_status()
 
-    def send_file(self, caption: str, file_path: str):
-        """
-        Отправка файла с подписью
-        """
+    def _send_document(self, caption: str, file_path: Path):
         url = f"{self.base_url}/sendDocument"
 
-        with open(file_path, "rb") as f:
-            files = {"document": f}
+        with file_path.open("rb") as f:
+            files = {
+                "document": (file_path.name, f),
+            }
             data = {
                 "chat_id": self.chat_id,
                 "caption": caption,
@@ -40,5 +48,6 @@ class TelegramNotifier:
 
             r = requests.post(url, data=data, files=files, timeout=60)
             r.raise_for_status()
+
 
 
